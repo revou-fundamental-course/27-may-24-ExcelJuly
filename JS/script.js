0fa4d972adfc683315c9a912c8b9b5e7365c77e6
ExcelJuly <excelpro90@gmail.com>
2024-06-06 19:37:02 +0700

    inisiasi config

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,24 @@
-// Ini JavaScript
+
+
+function formValidation() {
+    const nama = document.forms["message_form"]["nama"].value;
+    const tanhir = document.forms["message_form"]["tanhir"].value;
+    const laki_laki = document.forms["message_form"]["laki_laki"].value;
+    const messages = document.forms["message_form"]["messages"].value;
+
+    if (nama == "" || tanhir == "" || laki_laki == "" || messages == "") {
+        alert("Still error");
+    }
+    setSenderUI(nama, tanhir, laki-laki, messages);
+    
+}
+
+function setSenderUI(nama, tanhir, laki_laki, messages) {
+    document.getElementById("senderFN").innerHTML = nama;
+    document.getElementById("senderBD").innerHTML = tanhir;
+    document.getElementById("senderG").innerHTML = laki_laki;
+    document.getElementById("senderM").innerHTML = messages;
+}
+
+
+
